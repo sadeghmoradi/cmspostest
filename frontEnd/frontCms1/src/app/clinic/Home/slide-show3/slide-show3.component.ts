245ab5ea30d2563591ab16a8cafd841b89e8edcf
7 <7@kk.com>
2019-10-13 11:41:28 +0330

    add slide-show3

--- frontEnd/frontCms1/src/app/clinic/Home/slide-show3/slide-show3.component.ts
+++ frontEnd/frontCms1/src/app/clinic/Home/slide-show3/slide-show3.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slide-show3.component.scss']
 })
 export class SlideShow3Component implements OnInit {
+  images ={}
+  
+  list={}
+  constructor() { 
 
-  constructor() { }
+    // ,{txt:'ss',url:'../assets/A1.jpg'}
+    // ,{txt:'ss',url:'../assets/A2.jpg'}
+    // ,{txt:'ss',url:'../assets/A3.jpg'}
+  }
 
   ngOnInit() {
+    this.images = [1, 2, 3,4].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+      this.list =[{img:this.images[1], title:'xzczxczcz',sub:'ss1'}
+    ,{img:this.images[2], title:'xzczxczcz',sub:'ss2'}
+    ,{img:this.images[3], title:'xzczxczcz',sub:'ss3'}
+    // ,{img:this.images[4], title:'xzczxczcz',sub:'ss4'}
+    ]
   }
 
 }
